6f6a2d81feb2216507f003379e5e6e93bdeb9b9a
juliacanas <julia_cl90@homtail.com>
2019-10-08 12:48:44 +0200

    signup html y js hechos

---
+++ scripts/User.js
@@ -0,0 +1,12 @@
+'use strict';
+
+class User {
+  constructor(name, city, email, password, categoria) {
+    this.name = name;
+    this.city = city;
+    this.email = email;
+    this.password = password;
+    this.categoria = categoria;
+
+  }
+}
