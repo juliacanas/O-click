323abc939bb76a93aa73b6f8bb5ad93378e35bde
juliacanas <julia_cl90@homtail.com>
2019-10-09 12:21:06 +0200

    logo puesto, api js modificado, css avanzado

--- scripts/imageAPI.js
+++ scripts/imageAPI.js
@@ -1,23 +1,29 @@
 'use strict';
 
 
-
 const getImageApi = async () => {
     let imagesArray = [];
     const container = document.querySelector('.header-background');
-    const image = await fetch("https://picsum.photos/v2/list?page=12");
+    const image = await fetch("https://picsum.photos/v2/list?page=9&limit=100");
     const toJson = await image.json();
-    //console.log(toJson)
-    toJson.forEach(image => {
+    console.log(toJson)//promise (que es tot d'objectes)
+
+    toJson.forEach(image =>{
+        if (image.id === "859"){
+            imagesArray.push(image.download_url);
+        }
+    })
+
+    console.log(imagesArray)
+    /*toJson.forEach(image => {
         imagesArray.push(image.download_url);
-    });
+    });*/
 
     let randImg = imagesArray[Math.floor(Math.random()*imagesArray.length)];
 
     container.style.backgroundImage = `url("${randImg}")`;
-    //container.style.backgroundRepeat = 'no-repeat';
-    //container.style.backgroundSize = 'cover';
 
 }
 
 getImageApi();
+
